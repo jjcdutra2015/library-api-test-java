68a9b6f1ed6aed5a4dedac282b6d7b5814b6ef7f
Julio Dutra <jjcdutra@gmail.com>
2021-10-09 10:09:11 -0300

    first commit

---
+++ src/test/java/com/jjcdutra2015/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjcdutra2015.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
