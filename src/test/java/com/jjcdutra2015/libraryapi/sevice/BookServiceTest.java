1503c9eca1a5fedf1ef13afa2f27e8a09c5a22b2
Julio Dutra <jjcdutra@gmail.com>
2021-10-12 10:24:44 -0300

    Criando o teste da camada de serviço

---
+++ src/test/java/com/jjcdutra2015/libraryapi/sevice/BookServiceTest.java
@@ -0,0 +1,34 @@
+package com.jjcdutra2015.libraryapi.sevice;
+
+import com.jjcdutra2015.libraryapi.model.entity.Book;
+import com.jjcdutra2015.libraryapi.service.BookService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class BookServiceTest {
+
+    BookService service;
+
+    @Test
+    @DisplayName("Deve salvar um livro")
+    public void saveBookTes() {
+        //cenario
+        Book book = Book.builder().title("As aventuras").author("Fulano").isbn("123").build();
+
+        //execucao
+        Book savedBook = service.save(book);
+
+        //verificacao
+        assertThat(savedBook.getId()).isNotNull();
+        assertThat(savedBook.getTitle()).isEqualTo("As aventuras");
+        assertThat(savedBook.getAuthor()).isEqualTo("Fulano");
+        assertThat(savedBook.getIsbn()).isEqualTo("123");
+    }
+}
